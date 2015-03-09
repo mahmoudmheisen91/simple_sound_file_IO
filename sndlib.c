616073f1d1ff7f5807c045b0598be47cd58ccfdd
Mahmoud Mheisen <mahmoudmheisen91@gmail.com>
2015-03-09 07:09:30 +0100

    Add error msgs and comments

--- sndlib.c
+++ sndlib.c
@@ -1,95 +1,133 @@
-#include <stdio.h>      /* for fprintf and stderr */
-#include <stdlib.h>     /* for exit */
-#include <err.h>
-#include <fcntl.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+// Include header file:
 #include "sndlib.h"
 
-WAVFILE* wav_open(const char *file_name, const int mode) {
+// Open wav file function:
+// inputs:
+//   char *file_name : name of the wav file
+//   int mode        : opening mode
+// return:
+//   WAVFILE*        : pointer to the wav file
+WAVFILE* wav_open(char *file_name, int mode) {
+    // check if the mode is correct:
     if (mode != SFM_READ && mode != SFM_WRITE && mode != SFM_RDWR) {
-        fprintf(stderr, "Error mode type\n");
+        fprintf(stderr, "Error opening file: mode type is not correct\n");
         exit(EXIT_FAILURE); /* indicate failure.*/
     }
 
+    // create wav file object and fill it is fields:
     WAVFILE wav_file;
     wav_file.file_discriptor = open(file_name, mode);
-    wav_file.is_read = (mode == SFM_READ) ? True : False;
-    wav_file.is_write = (mode == SFM_WRITE) ? True : False;
+    wav_file.file_name = file_name;
+    wav_file.is_read   = (mode == SFM_READ  || mode == SFM_RDWR) ? 1 : 0;
+    wav_file.is_write  = (mode == SFM_WRITE || mode == SFM_RDWR) ? 1 : 0;
 
-    if (mode == SFM_RDWR) {
-        wav_file.is_read = True;
-        wav_file.is_write = True;
-    }
-
+    // create new file if the file does not exists and the mode is write or read/write:
     if ((wav_file.file_discriptor < 1) && (mode == SFM_WRITE || mode == SFM_RDWR)) {
         wav_file.file_discriptor = open(file_name, mode | O_CREAT | O_EXCL, 0666);
     }
 
+    // check if the file discriptor is correct:
     if (wav_file.file_discriptor < 1) {
         fprintf(stderr, "Error opening file: %s\n", file_name);
         exit(EXIT_FAILURE); /* indicate failure.*/
     }
 
+    // return pointer to the opened file:
     WAVFILE *wav_file2 = &wav_file;
     return wav_file2;
 }
 
+// Read wav file function:
+// inputs:
+//   WAVFILE *wav_file    : pointer to the wav file
+//   wav_header_t *header : data structure to store wav file header
+//   wav_data_t *data     : pointer where the data need to be stored
+// return:
+//   int                   : to indicate reading status
 int wav_read(WAVFILE *wav_file, wav_header_t *header, wav_data_t *data) {
-    if (wav_file->is_read == False) {
-        fprintf(stderr, "File is write only\n");
+    // check if the file opened for reading:
+    if (!wav_file->is_read) {
+        fprintf(stderr, "Error reading from file %s: File is write only\n", wav_file->file_name);
         exit(EXIT_FAILURE); /* indicate failure.*/
     }
 
-    free(*data);
+    // allocate data element:
     *data = (wav_data_t)malloc(header->data_chunk_size);
 
+    // read header of the wav file and check it's size:
     int header_size = read(wav_file->file_discriptor, header, sizeof(wav_header_t));
     if (header_size < sizeof(wav_header_t)) {
-        fprintf(stderr, "File broken header\n");
+        fprintf(stderr, "Error reading from file %s: broken header\n", wav_file->file_name);
         exit(EXIT_FAILURE); /* indicate failure.*/
     }
 
+    // check if the file is a wav file, by checking the chunk_id or the format:
     if (strncmp(header->chunk_id, "RIFF", 4) || strncmp(header->format, "WAVE", 4)) {
-        fprintf(stderr, "Not a wav file\n");
-        exit(EXIT_FAILURE); /* indicate failure.*/
-    }
-    if (header->audio_format != 1) {
-        fprintf(stderr, "Only PCM encoding supported\n");
+        fprintf(stderr, "Error reading from file %s: Not a wav file\n", wav_file->file_name);
         exit(EXIT_FAILURE); /* indicate failure.*/
     }
 
+    // check if the encoding is PCM format:
+    if (header->audio_format != 1) {
+        fprintf(stderr, "Error reading from file %s: only PCM encoding support\n", wav_file->file_name);
+        exit(EXIT_FAILURE); /* indicate failure.*/
+    }
+
+    // read data of the wav file and check it's size:
     int data_size = read(wav_file->file_discriptor, *data, header->data_chunk_size);
     if (data_size < sizeof(wav_header_t)) {
-        fprintf(stderr, "File broken data\n");
+        fprintf(stderr, "Error reading from file %s: broken data\n", wav_file->file_name);
         exit(EXIT_FAILURE); /* indicate failure.*/
     }
 
+    // indicate successfull reading:
     return 1;
 }
 
+// Write wav file function:
+// inputs:
+//   WAVFILE *wav_file    : pointer to the wav file
+//   wav_header_t *header : data structure contain wav file header
+//   wav_data_t *data     : pointer where to wav file data
+// return:
+//   int                   : to indicate writing status
 int wav_write(WAVFILE *wav_file, wav_header_t *header, wav_data_t data) {
-    if (wav_file->is_write == False) {
-        fprintf(stderr, "File is read only\n");
+    // check if the file opened for writing:
+    if (!wav_file->is_write) {
+        fprintf(stderr, "Error writing to file %s: File is read only\n", wav_file->file_name);
         exit(EXIT_FAILURE); /* indicate failure.*/
     }
 
+    // write header of the wav file and check it's size:
     int header_size = write(wav_file->file_discriptor, header, sizeof(wav_header_t));
     if (header_size < sizeof(wav_header_t)) {
-        fprintf(stderr, "Error writing header\n");
+        fprintf(stderr, "Error writing header to the file %s\n", wav_file->file_name);
         exit(EXIT_FAILURE); /* indicate failure.*/
     }
 
+    // write data of the wav file and check it's size:
     int data_size = write(wav_file->file_discriptor, data, header->data_chunk_size);
     if (data_size < header->data_chunk_size) {
-        fprintf(stderr, "Error writing data\n");
+        fprintf(stderr, "Error writing data to the file %s\n", wav_file->file_name);
         exit(EXIT_FAILURE); /* indicate failure.*/
     }
 
+    // indicate successfull writing:
     return 1;
 }
 
+// Close wav file function:
+// inputs:
+//   WAVFILE *wav_file : pointer to the wav file
+// return:
+//   int                : to indicate closing status
 int wav_close(WAVFILE *wav_file) {
-    close(wav_file);
+    // close the file and check if it is closed correctly:
+    if (!close(wav_file)) {
+        fprintf(stderr, "Error closing file\n");
+        exit(EXIT_FAILURE); /* indicate failure.*/
+    }
+
+    // indicate successfull closing:
+    return 1;
 }
--- sndlib.h
+++ sndlib.h
@@ -5,50 +5,50 @@
 #define SNDLIB_H_
 
     // Include Libraries:
-    #include <inttypes.h>
+    #include <stdio.h>
+    #include <stdlib.h>
     #include <fcntl.h>
+    #include <string.h>
 
     // Define Constants:
     #define SFM_READ   O_RDONLY  // read only mode
     #define SFM_WRITE  O_WRONLY  // write only mode
     #define SFM_RDWR   O_RDWR    // read/write mode
-    #define True       1
-    #define False      0
 
     // Type Definition:
-    typedef int boolean;
-    typedef int16_t* wav_data_t;
+    // wav file data type that contain pointer to the data:
+    typedef int* wav_data_t;
 
+    // WAVFILE type that contain information about the file itself, eg. discriptor, name, open mode:
     typedef struct _WAVFILE {
         int file_discriptor;
-        boolean is_read;
-        boolean is_write;
+        char* file_name;
+        int is_read;
+        int is_write;
     } WAVFILE;
 
+    // wav file header type that contain information about the wav signal, eg:
+    // number of channels, audio format, sampling frequency, data length, etc:
     typedef struct _wav_header_t {
-        char     chunk_id[4];
-        uint32_t chunk_size;
-        char     format[4];
-        char     format_chunk_id[4];
-        uint32_t format_chunk_size;
-        uint16_t audio_format;
-        uint16_t num_channels;
-        uint32_t sample_rate;
-        uint32_t byte_rate;
-        uint16_t block_align;
-        uint16_t bit_rate;
-        char     data_chunk_id[4];
-        uint32_t data_chunk_size;
+        char            chunk_id[4];
+        unsigned int    chunk_size : 32;
+        char            format[4];
+        char            format_chunk_id[4];
+        unsigned int    format_chunk_size : 32;
+        unsigned int    audio_format : 16;
+        unsigned int    num_channels : 16;
+        unsigned int    sample_rate : 32;
+        unsigned int    byte_rate : 32;
+        unsigned int    block_align : 16;
+        unsigned int    bit_rate : 16;
+        char            data_chunk_id[4];
+        unsigned int    data_chunk_size : 32;
     } wav_header_t;
 
     // Functions Prototypes:
-    WAVFILE *wav_open(const char *file_name, const int mode);
+    WAVFILE *wav_open(char *file_name, int mode);
     int wav_close(WAVFILE *wav_file);
     int wav_read(WAVFILE *wav_file, wav_header_t *header, wav_data_t *data);
-    int wav_read_header(char *file_name, wav_data_t* samples);
-    int wav_read_data(char *file_name, wav_data_t* samples);
     int wav_write(WAVFILE *wav_file, wav_header_t *header, wav_data_t data);
-    int wav_write_header(char *file_name, wav_data_t samples);
-    int wav_write_data(char *file_name, wav_data_t samples);
 
 #endif // SNDLIB_H_
